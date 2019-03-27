2fda966c3d5beb392e8f0c016246d101ec467f9c
Dane Miller <danemiller22@gmail.com>
2019-03-27 09:20:29 -0500

    Cross-Origin Resource Sharing

--- api/photos-gdrive/index.php
+++ api/photos-gdrive/index.php
@@ -1,3 +1,27 @@
+
+<?php
+// PHP code to enable CORS
+// SOURCE:  https://www.kerstner.at/2015/02/enabling-cross-origin-resource-sharing-cors-for-php/
+// array holding allowed Origin domains
+$allowedOrigins = array(
+	 '(http(s)://)?(www\.)?littleapplerenfest\.com'
+	,'(http(s)://)?(www\.)?littleapplerenfest\.s3\-website\-us\-west\-2\.amazonaws\.com'
+);
+if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
+	foreach ($allowedOrigins as $allowedOrigin) {
+		if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
+			header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+			//header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+			header('Access-Control-Allow-Methods: GET');
+			header('Access-Control-Max-Age: 1000');
+			header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+			break;
+		}
+	}
+}
+?>
+
+
 <!DOCTYPE HTML>
 <html>
 <head>
