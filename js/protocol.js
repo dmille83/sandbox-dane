bcf7d4953079f23593173ee71906cb9ae82fea5e
Dane Miller <danemiller22@gmail.com>
2020-11-10 11:21:56 -0600

    Update protocol.js

--- js/protocol.js
+++ js/protocol.js
@@ -1,7 +1,7 @@
 if (location.protocol.indexOf("https") === -1) {
 	if (location.protocol.indexOf("http") === 0) {
-		location.href = "https" + location.href.slice(4);
 		console.log("reloading page under https protocol");
+		location.href = "https" + location.href.slice(4);
 	} else {
 		console.log("unable to reload page under https protocol");	
 	}
