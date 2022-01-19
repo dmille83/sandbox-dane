89ee33ce14eec33e4413faa966adb20ab3dd29f4
Dane Miller <dmille83@ksu.edu>
2022-01-19 11:50:25 -0600

    Update lib.js

--- softdocs/lib.js
+++ softdocs/lib.js
@@ -1,5 +1,7 @@
 // To accomplish this you will need to use some custom JavaScript inside the' viewmodel.js' file of your form. 
-// Place the following inside the afterLoad() block. 
+// Place the following inside the afterLoad() block.
+
+// Format money with commas (text field)
 function initMoneyCommas() {
     function registerMoneyCommas() {
     	[].forEach.call(document.querySelectorAll('input[data-bind]'), function(element) {
@@ -28,4 +30,26 @@ function initMoneyCommas() {
     	element.addEventListener('click', function() { registerMoneyCommas(); }, false);
     });
     registerMoneyCommas();
+}
+
+// Format email address as ___@ksu.edu (text field)
+function initEmailKSU() {
+	function registerEmailKSU() {
+		[].forEach.call(document.querySelectorAll('.maskemail'), function(element) {
+			element.addEventListener('change', function() { formatEmailKSU(this); }, false);
+		});
+	}
+	function formatEmailKSU(elem) {
+		var val = elem.value;
+		if (val == null || val.toString() == "") { return; }
+		var parts = val.toString().split("@");
+		if (parts.length == 1 || parts[1].toString() == "k-state.edu") {
+			elem.value = parts[0] + "@ksu.edu";
+		}
+	}
+	[].forEach.call(document.querySelectorAll('button'), function(element) {
+		console.log(element.getAttribute('id') + '==' + element.getAttribute('data-bind'));
+		element.addEventListener('click', function() { registerEmailKSU(); }, false);
+	});
+	registerEmailKSU();
 }
