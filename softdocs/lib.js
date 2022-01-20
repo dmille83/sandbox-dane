5d5f15c92da54f6e918bd6e866aa564416d7542d
Dane Miller <dmille83@ksu.edu>
2022-01-20 11:39:33 -0600

    Update lib.js

--- softdocs/lib.js
+++ softdocs/lib.js
@@ -55,35 +55,3 @@ function initEmailKSU() {
 	});
 	registerEmailKSU();
 }
-
-// Return unique text array
-function uniqueTxtArray(txtArray) {
-	function onlyUnique(value, index, self) {
-		return self.indexOf(value) === index;
-	}
-	var unique = txtArray.filter(onlyUnique);
-	return unique;
-}
-
-// Return unique object array
-// Also works for a text array
-function uniqueObjArray(arr) {
-	var a = [];
-	var unique = [];
-	for (i = 0; i < arr.length; i++) {
-		var v = JSON.stringify(arr[i]);
-		if (a.indexOf(v) == -1) {
-			a.push(v);
-			var o = arr[i];
-			unique.push(o);
-		}
-	}
-	return unique;
-}
-
-// Prevents form suggestion 
-function disableAutocomplete() {
-	$( document ).on( 'focus', 'input', function(){ 
-		$( this ).attr( 'autocomplete', 'chrome-off' ); 
-	});
-}
