a7f1ecdcf72e26fa8f3c11a924aadff0030bc820
Dane Miller <dmille83@ksu.edu>
2022-01-20 12:57:25 -0600

    Update lib.js

--- softdocs/lib.js
+++ softdocs/lib.js
@@ -90,7 +90,11 @@ function disableAutocomplete() {
 
 function showFormLoadingMessage(boolEnable) {
 	if (boolEnable == true) {
-		$( 'body' ).prepend('<div id="softdocsLoadingBar" style="display: block; position: fixed; width: 50%; text-align: center; margin: 25% 25% 25% 25%; background: white; z-index: 999; font-size: 26px; font-weight: bold; padding: 20px; border: 4px solid black;">Loading form, please wait...</div>');
+		if (document.getElementById("softdocsLoadingBar")) {
+		   $( '#softdocsLoadingBar' ).css('display', '');
+		} else {
+			$( 'body' ).prepend('<div id="softdocsLoadingBar" style="display: block; position: fixed; width: 50%; text-align: center; margin: 25% 25% 25% 25%; background: white; z-index: 999; font-size: 26px; font-weight: bold; padding: 20px; border: 4px solid black;">Loading form, please wait...</div>');
+		}
 	} else {
 		$( '#softdocsLoadingBar' ).css('display', 'none');
 	}
